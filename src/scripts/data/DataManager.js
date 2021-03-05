8d616289c96e666a423d705ec28470aeeeed6e3b
David Darden <davidedarden83@gmail.com>
2021-03-05 15:36:28 -0600

    initial commit

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -7,7 +7,7 @@ export const getPosts = () => {
     return fetch("http://localhost:8088/posts")
     .then(response => response.json())
 }
-
+//COMMENT
 const loggedInUser = {
     id: 1,
     name: "David",

911651bb5170a46bcca06da854a16f6d75a24bee
David Darden <davidedarden83@gmail.com>
2021-03-10 11:39:56 -0600

    filtering posts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@
 
 
 // Can you explain what is being imported here?
-import { getPosts, getUsers } from "./data/DataManager.js"
+import { getPosts, getUsers, usePostCollection } from "./data/DataManager.js"
 import { NavBar } from "./nav/NavBar.js";
 import{PostList} from "./feed/PostList.js";
 import{Footer} from"./nav/Footer.js";
@@ -55,3 +55,25 @@ const showFooter = () => {
   footerElement.innerHTML = Footer();
 }
 showFooter();
+
+applicationElement.addEventListener("change", event => {
+  if (event.target.id === "yearSelection") {
+    const yearAsNumber = parseInt(event.target.value)
+    console.log(`User wants to see posts since ${yearAsNumber}`)
+    //invoke a filter function passing the year as an argument
+    showFilteredPosts(yearAsNumber);
+  }
+})
+
+const showFilteredPosts = (year) => {
+  //get a copy of the post collection
+  const epoch = Date.parse(`01/01/${year}`);
+  //filter the data
+  const filteredData = usePostCollection().filter(singlePost => {
+    if (singlePost.timestamp >= epoch) {
+      return singlePost
+    }
+  })
+  const postElement = document.querySelector(".postList");
+  postElement.innerHTML = PostList(filteredData);
+}
